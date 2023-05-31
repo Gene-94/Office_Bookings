a6466fbf9672d1d4262fc6e9fb452b7d54ab013b
Yevheniy <yevheniykushnirenko@gmail.com>
2023-05-31 06:31:10 +0100

    updated error handling

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -1,70 +1,68 @@
 import express from "express";
 import Hotel from "../models/Hotel.js"
+import { createError } from "../utils/error.js";
 
 const router = express.Router(); 
 
 
 //Create
-router.post("/", async (req, res)=>{
+router.post("/", async (req, res, next)=>{
 
-    const newHotel = new Hotel(req.body)
     try{
+        const newHotel = new Hotel(req.body)
         const savedHotel = await newHotel.save()
         res.status(200).json(savedHotel)
     }catch(err){
-        res.status(500).json(err)
+        next(err)
     }
 })
 
 //Read
-router.get("/:id", async (req, res) => {
+router.get("/:id", async (req, res, next) => {
     try{
         const hotel = await Hotel.findById(req.params.id)
-        if (!hotel){
-            res.status(404).json(`Hotel nº ${req.params.id} not found`)
-        }
-        else{
-            res.status(200).json(hotel)
-        }
+        if (!hotel) return next(createError(404, `Hotel nº ${req.params.id} not found`))
+        res.status(200).json(hotel)
+        
     }catch(err){
-        res.status(500).json(err)
+        next(err)
     }
 })
 
-router.get("/", async (req,res) => {
+router.get("/", async (req, res, next) => {
     try{
         const allHotels = await Hotel.find()
         res.status(200).json(allHotels)
     }catch(err){
-        res.status(500).json(err)
+        next(err)
     }
+    
 })
 
 //Update
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", async (req, res, next) => {
     try{
         const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {$set: req.body}, {new:true})
+        if (!updatedHotel) return next(createError(404, `Hotel nº ${req.params.id} not found`))
         res.status(200).json(updatedHotel)
     }catch(err){
-        res.status(500).json(err)
+        next(err)
     }
 })
 
 //Delete
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", async (req, re, next) => {
     try{
         const query = await Hotel.findByIdAndDelete(req.params.id)
-        if (!query){
-            res.status(404).json(`Hotel nº ${req.params.id} not found`)
-        }
-        else{
-            res.status(200).json(`Hotel nº ${req.params.id} deleted`)
-        }
+        if (!query) return next(createError(404, `Hotel nº ${req.params.id} not found`))
+        res.status(200).json(`Hotel nº ${req.params.id} deleted`)
+
     }catch(err){
-        res.status(500).json(err)
+        next(err)
     }
+    next()
 })
 
 
